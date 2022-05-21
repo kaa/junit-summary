d6d953d91ca84c13212510fd20a37079140343e4
Joakim Beijar <kaa@users.noreply.github.com>
2022-05-21 21:47:37 +0000

    First attempt

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const core = require('@actions/core');
-const wait = require('./wait');
-
+const { parse }= require('junit2json');
+const fs = require("fs");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const path = core.getInput('path');
+    core.info(`Loading ${path}`)
+    const data = fs.readFileSync(path)
+    const junit = await parse(data);
+    let summary = core.summary
+      .addHeading('Test Results');
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    console.log(junit.testsuite[0].testcase)
+    
+    for(let suite of junit.testsuite) {
+      summary = summary
+        .addTable(Array.from(testcaseToRows(suite.testcase)))
+    }
+    await summary.write()
   } catch (error) {
     core.setFailed(error.message);
+  }
+}
+
+function *testcaseToRows(testcase) {
+  let lastClassName = "";
+  for(let test of testcase) {
+    if(lastClassName != test.classname) {
+      lastClassName = test.classname;
+      yield [{header: true, data: test.classname, colspan:3}]
+    }
+    yield [test.failure?"❌":"✅", `<span style="color: red">${test.name}</span>`, `${test.time}s`]
   }
 }
 
